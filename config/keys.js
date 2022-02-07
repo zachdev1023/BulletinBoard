b1a6a55f788f8ae2ae3c2c5c1e7ea487a141bab0
Zach Lee <83812917+zachdev1023@users.noreply.github.com>
2022-02-07 15:36:03 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://zachdev:ulolspop123@cluster0.7nlup.mongodb.net/member?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<pass>@<MongoDBCluster>.7nlup.mongodb.net/<MongoDBCollection>?retryWrites=true&w=majority",
 };
