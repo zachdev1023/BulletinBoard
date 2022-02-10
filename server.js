f6bea8162d86af79b2b5efe5eb4f76043fb73452
Zirach Hangad <zirachdev@gmail.com>
2022-02-10 16:45:42 +0800

    Nothing to see here

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 //DB Config
-const db = require("./config/keys").mongoURI;
+const db = require("./keys.js").mongoURI;
 
 // Connect to Mongo
 mongoose
